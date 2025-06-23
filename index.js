bf893f696674c512e36583d2b963cb7e25f38943
Tony Lam <Tonylam750@outlook.com>
2025-06-23 22:58:56 +0200

    added copy hex to clipboard feature

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const getSchemeBtn = document.getElementById("get-scheme-btn")
-const colorPicker = document.getElementById("seed-picker")
-const getScheme = document.getElementById("colour-scheme")
+const colorPicker = document.getElementById("color-picker")
+const getScheme = document.getElementById("color-scheme")
 
 let modesArr = ["monochrome", "monochrome-dark", "monochrome-light",
                 "analogic", "complement", "analogic-complement", "triad", "quad"]
@@ -10,8 +10,22 @@ let options = ""
 modesArr.forEach(scheme =>{
     options+= `<option value=${scheme}>${scheme}</option>`
 })
+document.getElementById("color-scheme").innerHTML = options
 
-document.getElementById("colour-scheme").innerHTML = options
+function copyToClipBoard(){
+        document.querySelectorAll(".copy-hex").forEach(span =>{
+            span.addEventListener("click", function(){
+                navigator.clipboard.writeText(this.getAttribute("data-hex"))
+                .then(() =>{
+                    this.style.background ="rgba(24, 148, 197, 0.5)"
+                    setTimeout(() =>{
+                        this.style.background = ""
+                    }, 300)
+
+                })
+            })
+        })
+    }
 
 function renderScheme(){
   fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicker.value.replace('#', '')}&mode=${getScheme.value}&count=5`)
@@ -21,17 +35,22 @@ function renderScheme(){
         console.log(data.colors)
         let displayColoursSchemes = ""
         let displaySchemeHex = ""
-        schemeColors.forEach((colour) =>{
-            displayColoursSchemes+= `<div class="colour" style="background:${colour.hex.value}"></div>`
-            displaySchemeHex += `<div>${colour.hex.value}</div>`
+        schemeColors.forEach((color) =>{
+            displayColoursSchemes+= `<div class="color" style="background:${color.hex.value}"></div>`
+            displaySchemeHex += `
+            <div>
+                <span class="copy-hex" data-hex="${color.hex.value}" style="cursor:pointer">${color.hex.value}</span>
+            </div>`
         })
         document.getElementById("palette").innerHTML = displayColoursSchemes
         document.getElementById("footer").innerHTML = displaySchemeHex
-     })
+        copyToClipBoard()
+    })
 }
 
 getSchemeBtn.addEventListener("click", renderScheme)
 
 window.addEventListener("DOMContentLoaded",renderScheme)
+
 
 

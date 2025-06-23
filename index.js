44084ff48d546bc3437d11c18d67e526dfce303a
Tony Lam <Tonylam750@outlook.com>
2025-06-23 22:09:17 +0200

    fetched colour api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
-const getSchemeBtn = document.getElementById()
+const getSchemeBtn = document.getElementById("get-scheme-btn")
+const colorPicker = document.getElementById("seed-picker")
+const getScheme = document.getElementById("colour-scheme")
 
 let modesArr = ["monochrome", "monochrome-dark", "monochrome-light",
                 "analogic", "complement", "analogic-complement", "triad", "quad"]
@@ -11,15 +13,25 @@ modesArr.forEach(scheme =>{
 
 document.getElementById("colour-scheme").innerHTML = options
 
-
-colorPicker = document.getElementById("seed-picker")
-
-
-colorPicker,addEventListener("input", ()=>{
-    console.log(colorPicker.value)
-})
-
-
-fetch("https://www.thecolorapi.com/scheme?mode=monochrome")
+function renderScheme(){
+  fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicker.value.replace('#', '')}&mode=${getScheme.value}&count=5`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const schemeColors = data.colors
+        console.log(data.colors)
+        let displayColoursSchemes = ""
+        let displaySchemeHex = ""
+        schemeColors.forEach((colour) =>{
+            displayColoursSchemes+= `<div class="colour" style="background:${colour.hex.value}"></div>`
+            displaySchemeHex += `<div>${colour.hex.value}</div>`
+        })
+        document.getElementById("palette").innerHTML = displayColoursSchemes
+        document.getElementById("footer").innerHTML = displaySchemeHex
+     })
+}
+
+getSchemeBtn.addEventListener("click", renderScheme)
+
+window.addEventListener("DOMContentLoaded",renderScheme)
+
+
